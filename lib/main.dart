a3ba4a7586114203d427ca66ba956b7b182182e1
b.hatem <b.hatem@spherewebservices.com>
2023-11-23 20:43:25 +0100

    added app root widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medx/medxapp.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,17 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-    
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:,
+      home: const MedxApp(),
     );
   }
 }
---
+++ lib/medxapp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MedxApp extends StatefulWidget {
+  const MedxApp({Key? key}) : super(key: key);
+
+  @override
+  _MedxAppState createState() => _MedxAppState();
+}
+
+class _MedxAppState extends State<MedxApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
