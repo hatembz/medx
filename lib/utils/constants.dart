f5fbffc959fa4cfaede14ff761f2f594016bda79
b.hatem <b.hatem@spherewebservices.com>
2023-11-23 20:42:59 +0100

    added svg icoons and colors

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kBlueColor = Color(0xff2972FE);
+const kRedColor = Color(0xffFD4444);
+const kVioletColor = Color(0xFFA584FF);
+const kOrangeColor = Color(0xFFFF7854);
+const kPinkColor = Color(0xFFFD43B3);
+const kLightBlueColor = Color(0xFF00C9E4);
+const kYellowColor = Color(0xFFFDA625);
+const kGreenColor = Color(0xFF00CC6A);
