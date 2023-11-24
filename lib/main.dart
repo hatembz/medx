6d87c6373efc182cfbd942b30e481e351c2bb777
b.hatem <b.hatem@spherewebservices.com>
2023-11-24 11:35:42 +0100

    adding text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medx/medxapp.dart';
+import 'package:medx/utils/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: kBlueColor),
+          useMaterial3: true,
+          fontFamily: 'source-sans-pro',
+          textTheme: TextTheme(
+            bodyMedium: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 34,
+              color: Colors.black,
+            ),
+            headlineMedium: TextStyle(
+              color: Colors.black,
+              fontSize: 26,
+              fontWeight: FontWeight.w600,
+            ),
+            displaySmall: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+            labelSmall: TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+            titleSmall: TextStyle(
+              color: Colors.black,
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
+            titleMedium: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+            labelMedium: TextStyle(
+              fontSize: 18,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          )),
       home: const MedxApp(),
     );
   }
