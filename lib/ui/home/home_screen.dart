2f3de2011d157a0bb12a237a062d11e06a23ed31
b.hatem <b.hatem@spherewebservices.com>
2023-11-25 22:40:32 +0100

    seperated home screen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:get/get.dart';
 import 'package:medx/utils/constants.dart';
+import 'package:medx/utils/shared.dart';
 import 'package:medx/utils/svg_icons.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,61 +12,131 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        bottomNavigationBar: Container(
-      width: 428,
-      height: 91,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Color(0x196A769A),
-            blurRadius: 20,
-            offset: Offset(0, -4),
-            spreadRadius: 0,
+    return SafeArea(
+        child: Scaffold(
+            body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          gap(context),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/icons/logoBlue.png', width: 35, height: 35),
+              SizedBox(width: context.width * 0.03),
+              Expanded(
+                child: SizedBox(
+                  child: Text('Doctorek', style: context.textTheme.headlineMedium),
+                ),
+              ),
+              Spacer(),
+              Container(
+                  width: context.height * 0.05,
+                  height: context.height * 0.05,
+                  padding: const EdgeInsets.all(10),
+                  decoration: ShapeDecoration(
+                    color: kdarkblue,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                  ),
+                  child: Icon(MedxIcons.bell, size: 18, color: kBlueColor)),
+              const SizedBox(width: 16),
+              Container(
+                  width: context.height * 0.05,
+                  height: context.height * 0.05,
+                  padding: const EdgeInsets.all(10),
+                  decoration: ShapeDecoration(
+                    color: kdarkblue,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                  ),
+                  child: Icon(MedxIcons.heart, size: 18, color: kBlueColor)),
+            ],
+          ),
+          gap(context),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: 'Find',
+                  style: context.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w400)),
+              TextSpan(
+                  text: ' Your doctor',
+                  style: context.textTheme.titleLarge
+                      ?.copyWith(fontWeight: FontWeight.w400, color: Colors.grey))
+            ])),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: SearchBar(
+              hintText: 'Search',
+              // shadowColor: MaterialStatePropertyAll(),
+              surfaceTintColor: MaterialStatePropertyAll<Color>(Colors.grey),
+              side: MaterialStatePropertyAll(BorderSide(width: 1, color: Color(0xFFEAEEF2))),
+              elevation: MaterialStatePropertyAll<double>(0),
+              controller: controller,
+              constraints: BoxConstraints(maxHeight: 50, minHeight: 50),
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0)),
+              // onTap: () => controller.openView(),
+              // onChanged: (_) => controller.openView(),
+              trailing: [const Icon(Icons.search)],
+            ),
+          ),
+          gap(context),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Special Doctor', style: context.textTheme.bodyMedium),
+              TextButton(
+                  onPressed: () {},
+                  child: Text('View All',
+                      style: context.textTheme.bodyMedium?.copyWith(color: kBlueColor)))
+            ],
+          ),
+          gap(context),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Top Doctor', style: context.textTheme.bodyMedium),
+              TextButton(
+                  onPressed: () {},
+                  child: Text('View All',
+                      style: context.textTheme.bodyMedium?.copyWith(color: kBlueColor)))
+            ],
+          ),
+          gap(context),
+          Container(
+            width: 160,
+            height: 200,
+            decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                      width: 1,
+                      strokeAlign: BorderSide.strokeAlignCenter,
+                      color: Color(0xFFDADADA)),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                shadows: [
+                  BoxShadow(
+                      color: Color(0x0F000000),
+                      blurRadius: 60,
+                      offset: Offset(0, 10),
+                      spreadRadius: 0)
+                ]),
+            child: Column(
+              children: [],
+            ),
           )
         ],
       ),
-      child: GNav(
-          // rippleColor: Colors.grey[800], // tab button ripple color when pressed
-          // hoverColor: Colors.grey[700], // tab button hover color
-          haptic: true, // haptic feedback
-          tabBorderRadius: 15,
-          // tabActiveBorder: Border.all(color: kdarkblue, width: 1), // tab button border
-          // tabBorder: Border.all(color: kdarkblue, width: 1), // tab button border
-          // tabShadow: [
-          //   BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)
-          // ], // tab button shadow
-          curve: Curves.easeOutExpo, // tab animation curves
-          duration: Duration(milliseconds: 90), // tab animation duration
-          gap: 8, // the tab button gap between icon and text
-          color: kBlueColor, // unselected icon color
-          activeColor: kBlueColor, // selected icon and text color
-          iconSize: 24, // tab button icon size
-          tabBackgroundColor: kdarkblue, // selected tab background color
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          tabs: [
-            GButton(
-              icon: MedxIcons.homefilled,
-              text: 'Home',
-            ),
-            GButton(
-              icon: MedxIcons.schedual,
-              text: 'Likes',
-            ),
-            GButton(
-              icon: MedxIcons.tasks,
-              text: 'Search',
-            ),
-            GButton(
-              icon: MedxIcons.profile,
-              text: 'Profile',
-            )
-          ]),
-    ));
+    )));
   }
 }
